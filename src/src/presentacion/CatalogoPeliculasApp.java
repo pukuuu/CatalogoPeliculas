eb98ef9afeca795e86664616de4d5c8007c91cd6
Jordan Urzua <jordanpukuu@gmail.com>
2023-12-05 01:27:28 -0300

    update idk 2

---
+++ src/src/presentacion/CatalogoPeliculasApp.java
@@ -0,0 +1,68 @@
+package presentacion;
+
+import dominio.Pelicula;
+import servicio.IServicioPeliculas;
+import servicio.ServicioPeliculasLista;
+
+import java.util.Scanner;
+
+public class CatalogoPeliculasApp {
+    public static void main(String[] args) {
+        var salir = false;
+        var consola = new Scanner(System.in);
+        IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+
+        while (!salir){
+            try{
+               mostrarMenu();
+                System.out.print("Introduce una opcion: ");
+                salir = ejecutarOpciones(consola, servicioPeliculas);
+            }
+            catch (Exception e){
+                System.out.println("Ocurrio un error: "+e.getMessage());
+            }
+            System.out.println();
+        }//fin while
+        }
+
+    private static void mostrarMenu() {
+        System.out.println("""
+                **** Catalogo de peliculas ****
+                1. Agregar pe licula
+                2. Listar peliculas
+                3. Buscar pelicula
+                4. Salir
+                """);
+
+    }
+
+        private static boolean ejecutarOpciones(Scanner consola, IServicioPeliculas servicioPeliculas) {
+            var opcion = Integer.parseInt(consola.nextLine());
+            var salir = false;
+            switch (opcion){
+                case 1 -> {
+                    System.out.println("Introduce el nombre de la pelicula: ");
+                    var nombrePelicula = consola.nextLine();
+                    servicioPeliculas.agregarPelicula(new Pelicula(nombrePelicula));
+                }
+                case 2 -> {
+                    servicioPeliculas.listarPeliculas();
+                }
+                case 3 -> {
+                    System.out.println("Introduce el nombre de la pelicula a buscar: ");
+                    var buscar = consola.nextLine();
+                    servicioPeliculas.buscarPelicula(new Pelicula(buscar));
+                }
+                case 4 -> {
+                    System.out.println("Hasta pronto...");
+                    salir = true;
+                }
+                default -> {
+                    System.out.println("Opcion no reconocida");
+                }
+            }//final switch
+            return salir;
+        }
+
+
+}
--- src/src/presentacion/Main.java
+++
@@ -1,7 +0,0 @@
-package presentacion;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        }
-    }
