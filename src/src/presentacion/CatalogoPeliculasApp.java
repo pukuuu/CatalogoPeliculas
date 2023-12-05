98c1f5eff576462e876475b3de101f3c2cf31343
Jordan Urzua <jordanpukuu@gmail.com>
2023-12-05 01:38:52 -0300

    update idk numero idk

--- src/src/presentacion/CatalogoPeliculasApp.java
+++ src/src/presentacion/CatalogoPeliculasApp.java
@@ -2,6 +2,7 @@ package presentacion;
 
 import dominio.Pelicula;
 import servicio.IServicioPeliculas;
+import servicio.ServicioPeliculasArchivo;
 import servicio.ServicioPeliculasLista;
 
 import java.util.Scanner;
@@ -10,7 +11,8 @@ public class CatalogoPeliculasApp {
     public static void main(String[] args) {
         var salir = false;
         var consola = new Scanner(System.in);
-        IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+        //IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+        IServicioPeliculas servicioPeliculas = new ServicioPeliculasArchivo();
 
         while (!salir){
             try{
